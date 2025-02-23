a020ef9106aa484e0f4cf5a1816235687ed915be
Skyy Moore <124527392+mooreApps22@users.noreply.github.com>
2025-02-23 21:03:00 +0000

    Add files via upload

---
+++ CPP06/ex00/inc/data.h
@@ -0,0 +1,22 @@
+#ifndef DATA_H
+# define DATA_H
+
+/*
+	MANUAL:
+		https://en.cppreference.com/w/cpp/header	
+*/
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <fstream>
+# include <sstream>
+# include <cstring>
+# include <ctime>
+# include <cstdlib>
+# include <climits>
+# include <cctype>
+# include <exception>
+# include <new>
+
+#endif
---
+++ CPP07/ex01/src/main.cpp
@@ -0,0 +1,6 @@
+#include "../inc/data.h"
+
+int	main(int ac, char **av)
+{
+	return (0);
+}

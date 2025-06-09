fac80290bc5b9fb4e1aaa836f57b62a090284147
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-06-09 16:29:16 +0100

    Added some more tests.

--- cpp05/ex00/src/main.cpp
+++ cpp05/ex00/src/main.cpp
@@ -14,10 +14,31 @@ void	inspect(Bureaucrat b)
 int	main(void)
 {
 	Bureaucrat b("Frank", 150);
+
 	try
 	{
-		b = Bureaucrat("Frank", 2);
+		b = Bureaucrat("Frank", 1);
 		inspect(b);
+		for (int i = 0; i < 162; i++)
+		{
+			b.decrementGrade();
+			inspect(b);
+		}
+	}
+	catch (std::exception& e)
+	{
+		reportCatch(e);
+	}
+
+	try
+	{
+		b = Bureaucrat("Frank", 150);
+		inspect(b);
+		for (int i = 0; i < 162; i++)
+		{
+			b.incrementGrade();
+			inspect(b);
+		}
 	}
 	catch (std::exception& e)
 	{
